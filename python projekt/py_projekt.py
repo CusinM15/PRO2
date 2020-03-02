3c9cf1dbd6baffb42dc2388d90f92702df7d97ec
CusinM15 <44202906+CusinM15@users.noreply.github.com>
2020-03-02 18:41:08 +0100

    projekt

--- python projekt/py_projekt.py
+++ python projekt/py_projekt.py
@@ -2,34 +2,12 @@
 import requests
 import re
 
-'Kvakavajo žabe'
+neki = re.split(r'<PodatkiObjave>',requests.get('https://www.ajpes.si/ObjaveVPOFiles%5CObjaveVPO.xml').text)
 
-"""
-p2.3 - Game of Thrones characters
-"""
-
-chars = [re.sub(r'<.*?>', '', re.sub(r'<sup>\d</sup>', '', char)) for char in re.findall(r'<td><a href="/wiki/.+" title=".+">.+</a>.*</td>', re.split(r'<span class="mw-headline" id="Recurring_/_Guest_cast">Recurring / Guest cast</span>', requests.get('https://en.wikipedia.org/wiki/List_of_Game_of_Thrones_characters').text)[0])]
-
-print("{0:>30s} | {1:s}".format('Character', 'Actor/Actress'))
-for i in range(0, len(chars), 2):
-  print("{0:>30s} | {1:s}".format("'" + chars[i + 1] + "'", "'" + chars[i] + "'"))
-print()
-
-"""
-p2.4 - Movies and OMDb API
-"""
-
-OMBD_KEY = 'cd9ba7d7'
-
-def omdb(tit):
-  movie = requests.get('http://www.omdbapi.com/?t=' + re.sub(r'\s', '+', tit) + '&apikey=' + OMBD_KEY).json()
-  print("{0:>12s} | '{1:s}' ({2:d})".format('Title', movie['Title'], int(movie['Year'])))
-  print("{0:>12s} | '{1:s}'".format('Genre', movie['Genre']))
-  print("{0:>12s} | {1:.1f} ({2:,d} votes)".format('IMDb', float(movie['imdbRating']), int(movie['imdbVotes'].replace(',', ''))))
-  print("{0:>12s} | '{1:s}'".format('Language', movie['Language']))
-  print("{0:>12s} | '{1:s}'".format('Country', movie['Country']))
-  print("{0:>12s} | {1:s}\n".format('Runtime', movie['Runtime']))
-
-omdb('Bohemian Rhapsody')
-omdb('Guardians of the Galaxy')
-omdb('The Godfather')
+for i, dr in enumerate(neki):
+  znak = re.findall(r'<PodatekObjave ime=".+" opis=".+">.+</PodatekObjave>', dr)
+  for aa in znak:
+    aa = re.sub(r'<.*?>', '', aa)
+    print(aa)
+  if i > 10:
+    break
